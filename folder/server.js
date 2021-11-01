6d489da3e9c61fd6ffd04f7439a9d174af72e974
sameer Soni <sameer.soni9227@gmail.com>
2021-11-01 14:01:14 +0530

    test

---
+++ folder/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+var app = express()
+const cors = require('cors')
+app.use(cors({ origin: "*" }))
+app.use(bodyParser.json())
+const fs = require('fs');
+const contents = fs.readFileSync('./server.js', { encoding: 'base64' });
+console.log(contents);
+let token = "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";
+let user = "sameer227"
+//list all repo's
+
+
+app.post('/list', async (req, res) => {
+
+    await axios.get(`https://api.github.com/users/${user}/repos`, { data: "data" }, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(resp => {
+        console.log(resp);
+        res.send(resp.data);
+    })
+})
+
+app.post('/repo/create', async (req, res) => {
+
+    await axios.post(`https://api.github.com/user/repos`, { name: req.body.name }, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(resp => {
+        console.log(resp);
+        res.send(resp.data)
+    })
+})
+
+axios.put(`https://api.github.com/repos/${user}/PostManTest/contents/folder/server.js`, {
+    owner: user,
+    repo: 'PostManTest',
+    path: 'folder/server.js',
+    message: "test",
+    committer: {
+        name: "sameer Soni",
+        email: "sameer.soni9227@gmail.com"
+    },
+    "content": contents,
+    "branch": "master"
+}, {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+}).then(resp => {
+    console.log(resp);
+    // res.send(resp.data)
+})
+
+
+
+app.listen(5000, console.log("server is running"));
